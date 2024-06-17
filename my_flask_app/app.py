3f272ddb63c31fc4d9b1c216e89c751e689a2dbd
QMblC <kurtovnevg@gmail.com>
2024-06-17 20:48:55 +0500

    1213da

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -8,6 +8,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 @app.route('/')
 def hello_world():
     return 'Moe Flask приложение в контейнере Docker.'
+
+@app.route('/test')
+def test():
+    return "test"
  
 if __name__ == '__main__':
     app.run()
